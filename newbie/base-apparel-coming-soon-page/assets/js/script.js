9afd207bda358c1c0ac956b675092d27267e8416
jc-enriquez <john.christopher.enriquez2@gmail.com>
2023-11-09 22:25:54 +0800

    refactored form validation

--- newbie/base-apparel-coming-soon-page/assets/js/script.js
+++ newbie/base-apparel-coming-soon-page/assets/js/script.js
@@ -7,38 +7,37 @@ const message = document.querySelector(".form__input-message");
 const emailRegExp =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+const resetForm = function () {
+  message.textContent = "";
+  inputGroup.style.border = "1px solid var(--desaturated-red)";
+  errorIcon.style.display = "none";
+  inputEmail.value = "";
+};
+
 const formSubmit = function (e) {
   e.preventDefault();
+
   const isValid = inputEmail.value === 0 || emailRegExp.test(inputEmail.value);
 
-  if (!isValid) {
-    if (
-      inputGroup.classList.contains("valid") ||
-      errorIcon.classList.contains("visible") ||
-      message.classList.contains("visible", "success")
-    ) {
-      inputGroup.classList.remove("valid");
-      errorIcon.classList.remove("invisible");
-      message.classList.remove("invisible", "success");
+  const submitError = function () {
+    if (!isValid) {
+      inputGroup.style.border = "1px solid var(--soft-red)";
+      message.textContent = "Please provide a valid email address";
+      message.style.color = "var(--soft-red)";
+      errorIcon.style.display = "block";
+    } else {
+      inputGroup.style.border = "1px solid var(--green)";
+      message.textContent = "Email submitted!";
+      message.style.color = "var(--green)";
+      errorIcon.style.display = "none";
     }
-    inputGroup.classList.add("invalid");
-    errorIcon.classList.add("visible");
-    message.classList.add("visible", "error");
-    message.textContent = "Please provide a valid email address";
-  } else {
-    if (
-      inputGroup.classList.contains("invalid") ||
-      errorIcon.classList.contains("visible") ||
-      message.classList.contains("visible", "error")
-    ) {
-      inputGroup.classList.remove("invalid");
-      errorIcon.classList.remove("visible");
-      message.classList.remove("visible", "error");
-    }
-    inputGroup.classList.add("valid");
-    message.classList.add("visible", "success");
-    message.textContent = "Email address submitted";
-  }
+  };
+
+  submitError();
+
+  setTimeout(() => {
+    resetForm();
+  }, 3000);
 };
 
 formEl.addEventListener("submit", formSubmit);
