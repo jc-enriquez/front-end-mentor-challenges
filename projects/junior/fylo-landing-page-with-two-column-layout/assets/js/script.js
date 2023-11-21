e25499ab9fa88cbc4b5fe92c237c85d85bc2bfd3
jc-enriquez <john.christopher.enriquez2@gmail.com>
2023-11-21 21:31:18 +0800

    refactored form validation

--- projects/junior/fylo-landing-page-with-two-column-layout/assets/js/script.js
+++ projects/junior/fylo-landing-page-with-two-column-layout/assets/js/script.js
@@ -2,6 +2,9 @@
 
 const formHero = document.querySelector(".form--hero");
 const formEarly = document.querySelector(".form--early");
+const input = document.querySelectorAll(".input");
+const error = document.querySelector(".error");
+
 const inputHero = document.querySelector(".input--hero");
 const inputEarly = document.querySelector(".input--early");
 const errorHero = document.querySelector(".error--hero");
@@ -10,71 +13,59 @@ const errorEarly = document.querySelector(".error--early");
 const emailRegExp =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-const errorStyleHero = function () {
-  inputHero.style.border = "1px solid #ff6b6b";
-  errorHero.textContent = "Please check your email";
-  errorHero.style.color = "#ff6b6b";
-  errorHero.style.display = "block";
+let isValid;
+
+const errorStyle = function () {
+  if (input.classList.contains("input--hero")) {
+    inputHero.style.border = "1px solid #ff6b6b";
+    error.style.color = "#ff6b6b";
+  }
+
+  if (input.classList.contains("input--early")) {
+    input.style.boxShadow = "inset 0 0 0 1px #ff6b6b";
+    error.style.color = "#f8f8fe";
+  }
+
+  error.style.display = "block";
+  error.textContent = "Please check your email";
 };
 
-const errorStyleEarly = function () {
-  inputEarly.style.boxShadow = "inset 0 0 0 1px #ff6b6b";
-  errorEarly.textContent = "Please check your email";
-  errorEarly.style.color = "#f8f8fe";
-  errorEarly.style.display = "block";
+const successStyle = function () {
+  if (input.classList.contains("input--hero")) {
+    input.style.border = "1px solid #51cf66";
+    error.style.color = "#51cf66";
+  }
+
+  if (input.classList.contains("input--early")) {
+    input.style.boxShadow = "inset 0 0 0 1px #51cf66";
+    error.style.color = "#51cf66";
+  }
+
+  error.style.display = "block";
+  error.textContent = "Email submitted!";
 };
 
-const successStyleHero = function () {
-  inputHero.style.border = "1px solid #51cf66";
-  errorHero.textContent = "Email submitted!";
-  errorHero.style.color = "#51cf66";
-  errorHero.style.display = "block";
+const resetForm = function () {
+  input.classList.contains("input--hero")
+    ? (input.style.border = "1px solid hsl(0, 0%, 75%)")
+    : (input.style.boxShadow = "none");
+
+  input.value = "";
+  error.textContent = "";
+  error.style.display = "none";
 };
 
-const successStyleEarly = function () {
-  inputEarly.style.boxShadow = "inset 0 0 0 1px #51cf66";
-  errorEarly.textContent = "Email submitted!";
-  errorEarly.style.color = "#51cf66";
-  errorEarly.style.display = "block";
-};
-
-const resetFormHero = function () {
-  inputHero.style.border = "1px solid hsl(0, 0%, 75%)";
-  inputHero.value = "";
-  errorHero.textContent = "";
-  errorHero.style.display = "none";
-};
-
-const resetFormEarly = function () {
-  inputEarly.style.boxShadow = "none";
-  inputEarly.value = "";
-  errorEarly.textContent = "";
-  errorEarly.style.display = "none";
-};
-
-const formSubmitHero = function (e) {
+const formSubmit = function (e) {
   e.preventDefault();
 
-  const isValid = inputHero.value === 0 || emailRegExp.test(inputHero.value);
+  isValid = input.value === 0 || emailRegExp.test(input.value);
 
-  !isValid ? errorStyleHero() : successStyleHero();
+  !isValid ? errorStyle() : successStyle();
 
   setTimeout(() => {
-    resetFormHero();
+    resetForm();
   }, 3000);
 };
 
-const formSubmitEarly = function (e) {
-  e.preventDefault();
-
-  const isValid = inputEarly.value === 0 || emailRegExp.test(inputEarly.value);
-
-  !isValid ? errorStyleEarly() : successStyleEarly();
-
-  setTimeout(() => {
-    resetFormEarly();
-  }, 3000);
-};
-
-formHero.addEventListener("submit", formSubmitHero);
-formEarly.addEventListener("submit", formSubmitEarly);
+formHero.addEventListener("submit", formSubmit);
+formEarly.addEventListener("submit", formSubmit);
