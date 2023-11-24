d830a95dcf20d79d674e0e2dac33070acd4bfffa
jc-enriquez <john.christopher.enriquez2@gmail.com>
2023-11-24 20:24:10 +0800

    fixed accordion issue

--- projects/newbie/faq-accordion-card/assets/js/script.js
+++ projects/newbie/faq-accordion-card/assets/js/script.js
@@ -5,9 +5,8 @@ const accordionItems = document.querySelectorAll(".faq__accordion-item");
 
 accordion.addEventListener("click", function (e) {
   const clicked = e.target.closest(".faq__accordion-item");
-
-  // // Remove active class
-  // accordionItems.forEach((item) => item.classList.remove("active"));
+  // Remove active class
+  accordionItems.forEach((item) => item.classList.remove("active"));
 
   // Add active class
   clicked.classList.toggle("active");
