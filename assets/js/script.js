ea84cea626610567b5624df050dd23faf7c142cf
jc-enriquez <john.christopher.enriquez2@gmail.com>
2023-12-02 21:02:08 +0800

    added light mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 "use strict";
 
+// Setting year
 const year = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
-
 year.textContent = currentYear;
+
+// Setting light mode
+const body = document.querySelector("body");
+const btnSwitch = document.querySelector("button");
+const btnIcon = document.querySelector(".btn-icon");
+
+const switchBtnIcon = function () {
+  btnIcon.getAttribute("name") === "sunny"
+    ? btnIcon.setAttribute("name", "moon")
+    : btnIcon.setAttribute("name", "sunny");
+};
+
+const lightMode = function () {
+  body.classList.toggle("light-mode");
+  btnSwitch.classList.toggle("btn--light");
+  switchBtnIcon();
+};
+
+btnSwitch.addEventListener("click", lightMode);
