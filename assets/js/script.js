e4ee13ec40d1dce44878d1e2de127145d0cc7a46
jc-enriquez <john.christopher.enriquez2@gmail.com>
2023-12-28 15:36:59 +0800

    added functionalities

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,52 @@
 const containerProject = document.querySelector(".projects-container");
 const year = document.querySelector(".year");
 
-// Setting light mode
 const body = document.querySelector("body");
-const btnSwitch = document.querySelector("button");
-const btnIcon = document.querySelector(".btn-icon");
+const btnSwitch = document.querySelector(".btn--mode");
+const btnScroll = document.querySelector(".btn--scroll");
+const iconBtn = document.querySelector(".btn-icon");
 
+// Setting dark mode
 const switchBtnIcon = function () {
-  btnIcon.getAttribute("name") === "sunny"
-    ? btnIcon.setAttribute("name", "moon")
-    : btnIcon.setAttribute("name", "sunny");
+  iconBtn.getAttribute("name") === "sunny"
+    ? iconBtn.setAttribute("name", "moon")
+    : iconBtn.setAttribute("name", "sunny");
 };
 
-const lightMode = function () {
-  body.classList.toggle("light-mode");
+const darkMode = function () {
+  body.classList.toggle("dark-mode");
   btnSwitch.classList.toggle("btn--light");
   switchBtnIcon();
 };
 
-btnSwitch.addEventListener("click", lightMode);
+btnSwitch.addEventListener("click", darkMode);
+
+// Scroll to top
+const hideBtnScroll = function () {
+  btnScroll.style.display = "none";
+  btnScroll.style.visibility = "hidden";
+  btnScroll.style.opacity = 0;
+};
+
+const showBtnScroll = function () {
+  btnScroll.style.display = "flex";
+  btnScroll.style.visibility = "visible";
+  btnScroll.style.opacity = 1;
+};
+
+const watchScroll = function () {
+  document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
+    ? showBtnScroll()
+    : hideBtnScroll();
+};
+
+const scrollToTop = function () {
+  body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+window.addEventListener("scroll", watchScroll);
+btnScroll.addEventListener("click", scrollToTop);
 
 // Display Projects
 const projects = {
@@ -30,7 +58,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/product-preview-card-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/product-preview-card-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/product-preview-card-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -43,7 +71,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/results-summary-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/results-summary-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/results-summary-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -56,7 +84,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/order-summary-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/order-summary-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/order-summary-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -69,7 +97,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/order-summary-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/qr-code-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/qr-code-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -82,7 +110,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/nft-preview-card-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/nft-preview-card-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/nft-preview-card-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -95,7 +123,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/stats-preview-card-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/stats-preview-card-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/stats-preview-card-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -108,7 +136,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/three-column-preview-card-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/three-column-preview-card-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/three-column-preview-card-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -121,7 +149,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/profile-card-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/profile-card-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/profile-card-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -134,7 +162,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/social-proof-section",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/social-proof-section.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/social-proof-section.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -147,7 +175,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/four-card-feature-section",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/four-card-feature-section.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/four-card-feature-section.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -160,7 +188,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/single-price-grid-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/single-price-grid-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/single-price-grid-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -173,7 +201,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/huddle-landing-page-with-introductory-section",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/huddle-landing-page-with-single-introductory-section.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/huddle-landing-page-with-single-introductory-section.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -186,7 +214,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/interactive-rating-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/interactive-rating-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/interactive-rating-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -200,7 +228,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/article-preview-component",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/article-preview-component.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/article-preview-component.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -214,7 +242,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/ping-coming-soon-page",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/ping-coming-soon-page.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/ping-coming-soon-page.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -228,7 +256,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/base-apparel-coming-soon-page",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/base-apparel-coming-soon.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/base-apparel-coming-soon.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -242,7 +270,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/intro-component-with-signup-form",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/intro-component-with-signup-form.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/intro-component-with-signup-form.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -256,7 +284,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/newbie/faq-accordion-card",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/faq-accordion-card.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/faq-accordion-card.webp",
     difficulty: "Newbie",
     skills: {
       html: "HTML",
@@ -270,7 +298,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/junior/testimonials-grid-section",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/testimonials-grid-section.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/testimonials-grid-section.webp",
     difficulty: "Junior",
     skills: {
       html: "HTML",
@@ -284,7 +312,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/junior/huddle-landing-page-with-alternating-feature-blocks",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/huddle-landing-page-with-alternating-feature-blocks.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/huddle-landing-page-with-alternating-feature-blocks.webp",
     difficulty: "Junior",
     skills: {
       html: "HTML",
@@ -298,7 +326,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/junior/huddle-landing-page-with-curve-sections",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/huddle-landing-page-with-alternating-feature-blocks.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/huddle-landing-page-with-alternating-feature-blocks.webp",
     difficulty: "Junior",
     skills: {
       html: "HTML",
@@ -313,7 +341,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/junior/fylo-landing-page-with-two-column-layout",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/fylo-landing-age-with-two-column-layout.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/fylo-landing-page-with-two-column-layout.webp",
     difficulty: "Junior",
     skills: {
       html: "HTML",
@@ -328,7 +356,7 @@ const projects = {
     codeLink:
       "https://github.com/jc-ve/front-end-mentor-challenges/tree/main/projects/junior/news-homepage",
     imgLink:
-      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/news-homepage.jpg",
+      "https://cdn.jsdelivr.net/gh/jc-ve/images-thumbnail/front-end-mentor/news-homepage.webp",
     difficulty: "Junior",
     skills: {
       html: "HTML",
@@ -340,12 +368,21 @@ const projects = {
 };
 
 Object.values(projects).forEach((project) => {
+  let skillContainer = [];
+
+  Object.keys(project.skills).forEach((skill) => {
+    skillContainer.push(
+      `<span class="card__skill card__skill--${skill}">${skill.toUpperCase()}</span>`
+    );
+  });
+
   const html = `
   <div class="card card--${project.difficulty.toLowerCase()}">
     <img
       src="${project.imgLink}"
       alt="${project.title} preview image"
       class="card__img"
+      loading="lazy"
     />
 
     <div class="card__body">
@@ -353,12 +390,12 @@ Object.values(projects).forEach((project) => {
         >${project.difficulty}</span
       >
       <div class="card__skills">
-      ${Object.values(project.skills).forEach((skill) => {
-        console.log(skill);
-      })}
+        ${skillContainer.join(" ")}
       </div>
       <div class="card__text">
-        <a href="#" class="card__title">${project.title}</a>
+        <a href="${project.projLink}" target="_blank" class="card__title">${
+    project.title
+  }</a>
       </div>
       <div class="card__links">
         <a href="${
