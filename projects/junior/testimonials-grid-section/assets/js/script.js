c5d5b452e21e5bd80c183dd176789c74c41c0f0f
jc-enriquez <john.christopher.enriquez2@gmail.com>
2024-01-12 19:35:06 +0800

    refactored content

---
+++ projects/junior/testimonials-grid-section/assets/js/script.js
@@ -0,0 +1,28 @@
+"use script";
+
+import testimonial from "../data.json" assert { type: "json" };
+
+const containerTestimonials = document.querySelector(".testimonials");
+
+const displayTestimonials = () => {
+  testimonial.forEach(({ name, title, review, isColored, color, imgPath }) => {
+    const testimonialEl = `
+    <figure class="testimonials__card ${
+      isColored === true ? `testimonials__card--${color}` : ""
+    }">
+      <div class="testimonials__author">
+        <img src="${imgPath}" alt="${name}" class="testimonials__author-img"/>
+        <div>
+          <span class="testimonials__author-name">${name}</span>
+          <span class="testimonials__author-role">Verified Graduate</span>
+        </div>
+      </div>
+      <p class="testimonials__title">${title}</p>
+      <blockquote class="testimonials__review">${review}</blockquote>
+    </figure>
+    `;
+    containerTestimonials.insertAdjacentHTML("beforeend", testimonialEl);
+  });
+};
+
+displayTestimonials();
