ca887f80a770b331e0352d3db22e88352a438030
jc-enriquez <john.christopher.enriquez2@gmail.com>
2024-01-16 17:39:44 +0800

    added mobile nav functionality

--- projects/junior/intro-section-with-dropdown-navigation/assets/js/script.js
+++ projects/junior/intro-section-with-dropdown-navigation/assets/js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const navigationList = document.querySelector(".navigation__list");
+const navigation = document.querySelector(".navigation");
+const btnMobile = document.querySelector(".btn--mobile");
 
 const showDropDown = (el) => {
   const clicked = el.target.closest(".navigation__item--dropdown");
@@ -8,4 +10,10 @@ const showDropDown = (el) => {
   clicked.classList.toggle("active");
 };
 
+const showMobileNav = () => {
+  navigation.classList.toggle("active");
+};
+
 navigationList.addEventListener("click", showDropDown);
+
+btnMobile.addEventListener("click", showMobileNav);
