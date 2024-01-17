e7fcac772e93c0514948b856f52a1c1239e5facb
jc-enriquez <john.christopher.enriquez2@gmail.com>
2024-01-17 11:05:02 +0800

    fixed mobile issues

--- projects/junior/intro-section-with-dropdown-navigation/assets/js/script.js
+++ projects/junior/intro-section-with-dropdown-navigation/assets/js/script.js
@@ -3,6 +3,8 @@
 const navigationList = document.querySelector(".navigation__list");
 const navigation = document.querySelector(".navigation");
 const btnMobile = document.querySelector(".btn--mobile");
+const html = document.querySelector("html");
+const body = document.querySelector("body");
 
 const showDropDown = (el) => {
   const clicked = el.target.closest(".navigation__item--dropdown");
@@ -12,6 +14,9 @@ const showDropDown = (el) => {
 
 const showMobileNav = () => {
   navigation.classList.toggle("active");
+  html.classList.toggle("active");
+  html.classList.toggle("u-overflow-y-hidden");
+  body.classList.toggle("u-overflow-y-hidden");
 };
 
 navigationList.addEventListener("click", showDropDown);
