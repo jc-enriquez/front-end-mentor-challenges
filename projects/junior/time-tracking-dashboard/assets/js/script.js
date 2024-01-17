0c64f1c8b8182fc320c00ea9297df5926b73a877
jc-enriquez <john.christopher.enriquez2@gmail.com>
2024-01-17 22:06:27 +0800

    retrieved data from json

--- projects/junior/time-tracking-dashboard/assets/js/script.js
+++ projects/junior/time-tracking-dashboard/assets/js/script.js
@@ -1,3 +1,39 @@
 "use strict";
 
-const containerGrid = document.querySelector(".grid");
+import data from "../data.json" assert { type: "json" };
+console.log(data);
+
+const containerGrid = document.querySelector(".grid-stat");
+
+const displayData = () => {
+  data.forEach(({ title, timeframes }) => {
+    const dataEl = `
+    <figure class="card card--stat card--stat--${title
+      .split(" ")
+      .join("")
+      .toLowerCase()}">
+      <div class="card--stat__variant card--stat__variant--${title
+        .split(" ")
+        .join("")
+        .toLowerCase()}"></div>
+      <div class="card--stat__content">
+        <div class="card--stat__header">
+          <span class="card--stat__title">${title}</span>
+          <img
+            src="assets/images/icon-ellipsis.svg"
+            alt="Ellipsis icon"
+          />
+        </div>
+        <div class="card--stat__numbers">
+          <p class="card--stat__hours">${timeframes.weekly.current}hrs</p>
+          <p class="card--stat__previous-stat">Last week - ${
+            timeframes.weekly.previous
+          }hrs</p>
+        </div>
+      </div>
+    </figure>`;
+    containerGrid.insertAdjacentHTML("beforeend", dataEl);
+  });
+};
+
+displayData();
