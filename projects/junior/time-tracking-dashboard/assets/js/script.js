e3f55f7764d60b95d62ff7371ed46676a98f065f
jc-enriquez <john.christopher.enriquez2@gmail.com>
2024-01-18 16:07:58 +0800

    added set functionality

--- projects/junior/time-tracking-dashboard/assets/js/script.js
+++ projects/junior/time-tracking-dashboard/assets/js/script.js
@@ -1,21 +1,18 @@
 "use strict";
 
 import data from "../data.json" assert { type: "json" };
-console.log(data);
 
 const containerGrid = document.querySelector(".grid-stat");
+const cardList = document.querySelector(".card__list");
+const cardListItems = document.querySelectorAll(".card__list__item");
 
-const displayData = () => {
-  data.forEach(({ title, timeframes }) => {
+const displayData = (time = "weekly") => {
+  data.forEach(({ title, timeframes: { weekly, daily, monthly } }) => {
     const dataEl = `
     <figure class="card card--stat card--stat--${title
       .split(" ")
       .join("")
       .toLowerCase()}">
-      <div class="card--stat__variant card--stat__variant--${title
-        .split(" ")
-        .join("")
-        .toLowerCase()}"></div>
       <div class="card--stat__content">
         <div class="card--stat__header">
           <span class="card--stat__title">${title}</span>
@@ -25,10 +22,32 @@ const displayData = () => {
           />
         </div>
         <div class="card--stat__numbers">
-          <p class="card--stat__hours">${timeframes.weekly.current}hrs</p>
-          <p class="card--stat__previous-stat">Last week - ${
-            timeframes.weekly.previous
+          <p class="card--stat__hours">${
+            time === "weekly"
+              ? weekly.current
+              : time === "daily"
+              ? daily.current
+              : time === "monthly"
+              ? monthly.current
+              : weekly.current
           }hrs</p>
+          <p class="card--stat__previous-stat">${
+            time === "weekly"
+              ? "Last week"
+              : time === "daily"
+              ? "Yesterday"
+              : time === "monthly"
+              ? "Last month"
+              : "Last week"
+          } - ${
+      time === "weekly"
+        ? weekly.previous
+        : time === "daily"
+        ? daily.previous
+        : time === "monthly"
+        ? monthly.previous
+        : weekly.previous
+    }hrs</p>
         </div>
       </div>
     </figure>`;
@@ -37,3 +56,21 @@ const displayData = () => {
 };
 
 displayData();
+
+const setData = (e) => {
+  const clicked = e.target.closest(".card__list__item");
+  const clickedContent = clicked.textContent.split(" ").join("").toLowerCase();
+
+  // Remove active class
+  cardListItems.forEach((item) => item.classList.remove("active"));
+
+  // Add active class
+  clicked.classList.add("active");
+
+  // Reset grid content
+  containerGrid.innerHTML = "";
+
+  displayData(clickedContent);
+};
+
+cardList.addEventListener("click", setData);
