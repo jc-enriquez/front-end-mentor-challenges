876e0ab7fdb3f712fbf079d2c2f534dd8ca33ab3
jc-enriquez <john.christopher.enriquez2@gmail.com>
2024-01-20 12:24:41 +0800

    fixed mobile issue

--- projects/junior/news-homepage/assets/js/script.js
+++ projects/junior/news-homepage/assets/js/script.js
@@ -3,10 +3,12 @@
 const btnMobile = document.querySelector(".btn--mobile");
 const navigation = document.querySelector(".navigation");
 const overlay = document.querySelector(".overlay");
+const html = document.querySelector("html");
 const body = document.querySelector("body");
 
 btnMobile.addEventListener("click", function () {
   navigation.classList.toggle("navigation__open");
+  html.classList.toggle("u-overflow-y-hidden");
   body.classList.toggle("u-overflow-y-hidden");
   overlay.classList.toggle("hidden");
 });
