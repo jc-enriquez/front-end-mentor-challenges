5dcd13e20995db899a6ce7b33d320a2e00e77cdc
jc-enriquez <john.christopher.enriquez2@gmail.com>
2024-01-24 10:54:30 +0800

    finished project

--- projects/junior/notifications-page/assets/js/script.js
+++ projects/junior/notifications-page/assets/js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const btnReadAll = document.querySelector(".btn--notification");
+const unreadNotifications = document.querySelectorAll(
+  ".notification__list__item--unread"
+);
+const unreadSymbols = document.querySelectorAll(".notification__symbol");
+const notificationTotal = document.querySelector(".notification__total");
+
+const removeUnreadStyle = () => {
+  notificationTotal.textContent = 0;
+  unreadNotifications.forEach((notification) => {
+    notification.classList.remove("notification__list__item--unread");
+  });
+  unreadSymbols.forEach((symbol) => {
+    symbol.classList.add("u-hidden");
+  });
+};
+
+btnReadAll.addEventListener("click", removeUnreadStyle);
