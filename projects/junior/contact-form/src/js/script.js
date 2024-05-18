c20d4244d5331cc72b5c1eb981c08e174708c542
jc-ve <john.christopher.enriquez.2@gmail.com>
2024-05-18 23:15:52 +0800

    added textarea & checkbox

--- projects/junior/contact-form/src/js/script.js
+++ projects/junior/contact-form/src/js/script.js
@@ -4,6 +4,7 @@ const form = document.getElementById("form");
 const inputFirstName = document.getElementById("first-name");
 const inputLastName = document.getElementById("last-name");
 const inputEmail = document.getElementById("email");
+const inputMessage = document.getElementById("message");
 
 const validateForm = (e) => {
   e.preventDefault();
